1d8d4e2ba0e2ccefd57d6530734ee0cc2920ef58
Mao <lin.maoxian0991@gmail.com>
2020-06-12 17:10:10 -0400

    Added table columns to accounts payable invoices model. Updated customers table model

--- models/accounts_payable_Invoices.js
+++ models/accounts_payable_Invoices.js
@@ -1,5 +1,42 @@
 module.exports = function ( sequelize, DataTypes){
-    var Accounts_Payable_Invoices = sequelize.define("Accounts_Payable_Invoices", {
-        
-    })
-}
+    var accounts_payable_invoices = sequelize.define("accounts_payable_invoices", {
+        invoice_number: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        supplier_id: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        purchase_order_number: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        invoice_date: {
+            type: DataTypes.DATEONLY,
+            allowNull: true
+        },
+        due_date: {
+            type: DataTypes.DATEONLY,
+            allowNull: true
+        },
+        receive_date: {
+            type: DataTypes.DATEONLY,
+            allowNull: false
+        },
+        account_number: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        invoice_total: {
+            type: DataTypes.DECIMAL(10,2),
+            allowNull: true
+        },
+        payment_status: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            defaultValue: "UNPAID"
+        }
+    });
+    return accounts_payable_invoices;
+};
---
+++ models/customers.js
@@ -0,0 +1,114 @@
+//TO DO
+//-Check sequelize for parameters to check for unquie fields.
+//
+module.exports = function ( sequelize, DataTypes){
+    var customers_database = sequelize.define("customers_database", {
+        customer_account_number: {
+            //customer account number is the same as business phone number
+            type: DataTypes.INTEGER,
+            required: true,
+            allowNUll: false,
+            unquie: true
+        },
+        customer_representative: {
+            type: DataTypes.STRING,
+            allowNUll: true
+        },
+        price_tier: {
+            type: DataTypes.INTEGER,
+            defaultValue: 1,
+        },
+        restaurant_name_english: {
+            type: DataTypes.STRING,
+            allowNUll: true,
+        },
+        restaurant_name_chinese: {
+            type: DataTypes.STRING,
+            allowNUll: true
+        },
+        contact_first_name: {
+            type: DataTypes.STRING,
+            allowNUll: true
+        },
+        contact_last_name: {
+            type: DataTypes.STRING,
+            allowNUll: true
+        },
+        delivery_street: {
+            type: DataTypes.STRING,
+            allowNUll: true
+        },
+        delivery_city: {
+            type: DataTypes.STRING,
+            allowNUll: true
+        },
+        delivery_state: {
+            type: DataTypes.STRING,
+            allowNUll: true
+        },
+        delivery_zipcode: {
+            type: DataTypes.INTEGER,
+            allowNUll: true
+        },
+        billing_street: {
+            type: DataTypes.STRING,
+            allowNUll: true
+        },
+        billing_city: {
+            type: DataTypes.STRING,
+            allowNUll: true
+        },
+        billing_state: {
+            type: DataTypes.STRING,
+            allowNUll: true
+        },
+        billing_zipcode: {
+            type: DataTypes.INTEGER,
+            allowNUll: true
+        },
+        business_phone_number: {
+            type: DataTypes.INTEGER,
+            required: true,
+            allowNUll: false
+        },
+        contact_phone_number: {
+            type: DataTypes.INTEGER,
+            allowNUll: false
+        },
+        customer_email: {
+            type: DataTypes.STRING,
+            allowNUll: true,
+            validate: {
+                isEmail: true
+              }
+        },
+        order_day_one: {
+            type: DataTypes.STRING,
+            allowNUll: true,
+        },
+        delivery_day_one: {
+            type: DataTypes.STRING,
+            allowNUll: true,
+        },
+        order_day_two: {
+            type: DataTypes.STRING,
+            allowNUll: true,
+        },
+        delivery_day_two: {
+            type: DataTypes.STRING,
+            allowNUll: true,
+        },
+        order_day_three: {
+            type: DataTypes.STRING,
+            allowNUll: true,
+        },
+        delivery_day_three: {
+            type: DataTypes.STRING,
+            allowNUll: true,
+        }
+    },{
+    //Stops sequelize auto-pluralization and keeps table name as model name.
+    freezeTableName: true
+    });
+    return customers_database;
+};
