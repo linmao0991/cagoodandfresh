dcae316ff3769b05d22f059ee699bb7bbcaf8eda
Mao <lin.maoxian0991@gmail.com>
2020-08-17 22:11:17 -0400

    Added log in functionality needs to be finished with a api call to server

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,18 +1,51 @@
-import React, {useContext} from "react";
+import React, {useState, useContext} from "react";
 import LoginContext from "../Context/LoginContext";
+
 
 function Login(){
     const loginContext = useContext(LoginContext);
+    const [userName, userNameInput] = useState("");
+    const [password, passwordInput] = useState("");
+
+    const logUserIn = () => {
+        console.log("username: " + userName);
+        console.log("password: " + password)
+        //API Call to log in using userName & password
+        //--Code Here
+
+        //On successful log in
+        //--Clear state values
+        // call loginContext.login()
+        loginContext.login()
+
+        //On unsucessful log in
+        //-- Clear state values
+        //-- Display unsuccessful log in
+    };
 
     return(
         <div>
-            <h1>Log In</h1>
-            <p>Username</p>
-            <input></input>
-            <p>Password</p>
-            <input></input>
-            <br />
-            <button onClick={loginContext.login}>Log In</button>
+            <form>
+                <h1>Log In</h1>
+                <p>Username</p>
+                <input
+                    value = {userName}
+                    type = "text"
+                    autoComplete= "username"
+                    onChange = {event => userNameInput(event.target.value)}
+                    className="form-control validate"
+                />
+                <p>Password</p>
+                <input
+                    value = {password}
+                    type = "password"
+                    autoComplete= "current-password"
+                    onChange = {event => passwordInput(event.target.value)}
+                    className="form-control validate"
+                />
+                <br />
+                <button onClick={() => logUserIn()}>Log In</button>
+            </form>
         </div>
     );
 }
