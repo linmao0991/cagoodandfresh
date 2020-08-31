e7a3f289964d26ce9072a40c04c98805c04f481d
linmao0991 <lin.maoxian0991@gmail.com>
2020-08-31 17:03:57 -0400

    Converting ProductSelection to functional component and moving some functions to OrderForm

--- client/src/Components/ProductSelection/ProductSelection.js
+++ client/src/Components/ProductSelection/ProductSelection.js
@@ -6,7 +6,7 @@ import Api from "../../Utils/Api";
 //--Figure out how to call server for categories before rendering selection on the UI
 //-----Got function to call server, if you setState to store server data, it will infinitly. Need to figure out a solution
 //--Figure out how to layout the category tree holding -> category -> Products
-//-----Render button based on categories returned from server data. Then call server to get products in category and render button or card.
+//-----Render button based on categories returned from server data. Then call server to get products in category and render button or card.g
 //--Add a functions to select product and store it to the context, then have order cart render the ordercart context. 
 
 class ProductSelection extends Component{
@@ -20,19 +20,17 @@ class ProductSelection extends Component{
             //this.setState({productCate: this.context.productCate})
             console.log(this.context.productCate)
             return(
-                <div></div>
+                <div>Product Selection</div>
             )
         }else{
         Api.getProductCate().then(data => {
             //this.setState({productCate: data});
             let categories = []
             for (const index in data.data){
-                console.log("=================")
-                console.log(index)
-                console.log(data.data[index])
                 categories.push(data.data[index].category)
             }
             console.log(categories)
+            this.setState({productCate: categories})
             return(
                 <div>Product Selection</div>
             )
