a714894bee5eeec514e833b421dd393c206f0db0
linmao0991 <lin.maoxian0991@gmail.com>
2020-11-23 07:30:48 -0500

    Fixing folders

--- client/src/context/DirectoryContext.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const directoryContext = React.createContext({
-    currentDir: '',
-    previousDir: "none",
-    switchDir: () => {}
-})
-
-export default directoryContext
--- client/src/context/LoginContext.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const loginContext = React.createContext({
-    isLoggedin: false,
-    permissionLevel: 0,
-    login: () => {}
-})
-
-export default loginContext
