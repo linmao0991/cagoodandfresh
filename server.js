7f4dfc277be8265473275d4bdff4028ad8430d65
linmao0991 <lin.maoxian0991@gmail.com>
2020-11-23 07:47:00 -0500

    Rremoved proy from client package.json to attempt fix for invalid host header

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ var db = require("./models");
 // Creating express app and configuring middleware needed for authentication
 var app = express();
 app.use(express.urlencoded({ extended: true }));
-//app.use(express.json());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
