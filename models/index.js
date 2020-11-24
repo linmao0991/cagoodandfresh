47c07e8de28c2d9fe1d96adfa77d31f8f24c71a5
linmao0991 <lin.maoxian0991@gmail.com>
2020-11-23 22:28:33 -0500

    fixed path to sequelize config file

--- models/index.js
+++ models/index.js
@@ -9,13 +9,6 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + "/../config/config.js")[env];
 const db = {};
 
-if (process.env.NODE_ENV === "production") {
-  config = require(__dirname + "/../config/config.js")[env];
-}
-// else{
-//   config = require(__dirname + '/../config/config.json')[env];
-// }
-
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
